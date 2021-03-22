d9021365829620d5923c7feb351c69ca3143bbc7
amitkrmaharana <ak657harley@gmail.com>
2021-03-22 07:52:28 +0530

    Refactored: Added premium rides rates and calculated total fare

--- src/main/java/invoice/InvoiceGenerator.java
+++ src/main/java/invoice/InvoiceGenerator.java
@@ -2,9 +2,16 @@ package invoice;
 
 public class InvoiceGenerator {
 
+    public enum Ride_Type {
+        NORMAL_RIDE,PREMIUM_RIDE
+    }
+
     private static final double MINIMUM_COST_PER_KM = 10;
     private static final int COST_PER_TIME = 1;
     private static final double MINIMUM_FARE = 5;
+    private static final double MINIMUM_COST_PER_KM_PREMIUM_RIDE = 15;
+    private static final int COST_PER_TIME_PREMIUM_RIDE = 2;
+    private static final double MINIMUM_FARE_PREMIUM_RIDE = 20;
 
     public double calculateTotalFare(double distance, int time) {
         double totalFare = distance * MINIMUM_COST_PER_KM + time * COST_PER_TIME;
@@ -25,5 +32,18 @@ public class InvoiceGenerator {
             if (summaryWithId.getId() == id ) result = summaryWithId;
         }
         return result;
+    }
+
+    public double calculateTotalFare(double distance, int time, Ride_Type type) {
+        double totalFare = 0;
+        switch (type) {
+            case NORMAL_RIDE:
+                totalFare = Math.max(distance * MINIMUM_COST_PER_KM + time * COST_PER_TIME, MINIMUM_FARE);
+                break;
+            case PREMIUM_RIDE:
+                totalFare = Math.max(distance * MINIMUM_COST_PER_KM_PREMIUM_RIDE + time * COST_PER_TIME_PREMIUM_RIDE, MINIMUM_FARE_PREMIUM_RIDE);
+                break;
+        }
+        return totalFare;
     }
 }
--- src/test/java/invoice/InvoiceServiceTest.java
+++ src/test/java/invoice/InvoiceServiceTest.java
@@ -4,7 +4,6 @@ package invoice;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.jupiter.api.Assertions;
 
 public class InvoiceServiceTest {
     InvoiceGenerator invoiceGenerator;
@@ -27,13 +26,13 @@ public class InvoiceServiceTest {
         Assert.assertEquals(5,totalFare,0.0);
     }
     @Test
-    public void givenDistanceAndTImeWithMultipleRidesCount_ShouldReturnInvoiceSummary() {
+    public void givenDistanceAndTIme_WithMultipleRidesCount_ShouldReturnInvoiceSummary() {
         Ride[] ride1 = {new Ride(2.0, 5),
                 new Ride(0.1, 1)
         };
         InvoiceSummary summary = invoiceGenerator.calculateFare(ride1);
         InvoiceSummary expectedInvoiceSummary = new InvoiceSummary(2, 30.0);
-        Assertions.assertEquals(expectedInvoiceSummary, summary);
+        Assert.assertEquals(expectedInvoiceSummary, summary);
     }
     @Test
     public void givenUserId_ShouldReturn_InvoiceSummaryWithId() {
@@ -44,6 +43,22 @@ public class InvoiceServiceTest {
         InvoiceSummaryWithId summary = invoiceGenerator.invoiceList(summary1, id);
         InvoiceSummaryWithId expectedInvoiceSummary = new InvoiceSummaryWithId(1,30.2,1);
         Assert.assertEquals(expectedInvoiceSummary,summary);
+    }
+
+    @Test
+    public void givenDistanceAndTime_ForPremiumRide_ShouldReturn_TotalFare() {
+        double distance = 2.0;
+        int time = 5;
+        double fare = invoiceGenerator.calculateTotalFare(distance, time, InvoiceGenerator.Ride_Type.PREMIUM_RIDE);
+        Assert.assertEquals(40,fare,0.0);
+    }
+
+    @Test
+    public void givenDistanceAndTime_ForNormalRide_ShouldReturn_TotalFare() {
+        double distance = 2.0;
+        int time = 5;
+        double fare = invoiceGenerator.calculateTotalFare(distance, time, InvoiceGenerator.Ride_Type.NORMAL_RIDE);
+        Assert.assertEquals(25,fare,0.0);
     }
 
 }
